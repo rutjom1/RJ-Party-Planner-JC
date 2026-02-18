7f9c9b01f6a9f69d6df02c6c1cc2ce8220a04cc9
Ruth Jomy <rutjom1@gmail.com>
2026-02-18 16:01:43 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,10 @@ import type { FirebaseOptions } from 'firebase/app';
 // You should replace this with your actual Firebase project configuration.
 // You can get this from the Firebase console.
 export const firebaseConfig: FirebaseOptions = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID",
+    apiKey: "AIzaSyACh5lFhU9BWtWEa6l4WVTtSS1VJcuzn90",
+    authDomain: "studio-2419555289-92ac1.firebaseapp.com",
+    projectId: "studio-2419555289-92ac1",
+    storageBucket: "studio-2419555289-92ac1.appspot.com",
+    messagingSenderId: "1030188245249",
+    appId: "1:1030188245249:web:99a86b5b036335d8a57b00",
 };
